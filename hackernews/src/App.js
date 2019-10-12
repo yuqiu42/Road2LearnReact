85f9f9d460054170e9c8ed9c74af87e4d64fc861
YQ <yuqiu42@gmail.com>
2019-10-12 16:33:23 -0400

    First JSX variable

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -3,21 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const helloWorld = "Welcome to the Road to learn React";
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>{helloWorld}</h2>
       </header>
     </div>
   );
