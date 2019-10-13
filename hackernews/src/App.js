161192eabf91071e8f4607ab9e3d33620ed1c546
YQ <yuqiu42@gmail.com>
2019-10-13 17:13:19 -0400

    Display a list of pages

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -2,13 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const pages = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/', author: 'Jordan Walke', num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/', author: 'Dan Abramov, Andrew Clark', num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 function App() {
   const helloWorld = "Welcome to the Road to learn React";
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>{helloWorld}</h2>
-      </header>
+      {pages.map((page) => (
+        <div key={page.objectID}>
+          <span>
+            <a href={page.url}>{page.title}</a>
+          </span>
+          <span>{page.author}</span>
+          <span>{page.num_comments}</span>
+          <span>{page.points}</span>
+        </div>))}
     </div>
   );
 }
