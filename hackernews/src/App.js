bc91236b561917a9152d7eb6b0c57b5112a8cdd9
YQ <yuqiu42@gmail.com>
2019-10-26 23:06:05 -0400

    Use conditional rendering

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -96,8 +96,6 @@ class App extends Component {
 
   render() {
     const { searchTerm, result } = this.state;
-    if (!result) { return null; }
-
     return (
       <div className="page">
         <div className="interactions">
@@ -108,11 +106,12 @@ class App extends Component {
             Search:
           </Search>
         </div>
-        <Table
-          pages={result.hits}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
+        { result &&
+          <Table
+            pages={result.hits}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          /> }
       </div>
     );
   }
