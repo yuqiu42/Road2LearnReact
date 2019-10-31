bbb3dc8d681958f7f793b0140cc3e67f64a7c6f1
YQ <yuqiu42@gmail.com>
2019-10-31 19:38:38 -0400

    Add enzyme test for Table

--- hackernews/src/App.test.js
+++ hackernews/src/App.test.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 import App, { Search, Button, Table } from "./App";
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe("App", () => {
   it("renders without crashing", () => {
@@ -62,5 +66,10 @@ describe("Table", () => {
     const component = renderer.create(<Table {...props} />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("shows two items in the list", () => {
+    const element = shallow(<Table {...props} />);
+    expect(element.find(".table-row").length).toBe(2);
   });
 });
