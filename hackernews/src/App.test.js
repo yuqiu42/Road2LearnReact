2863db62468aed4b1eb8041474212067a7acdddb
YQ <yuqiu42@gmail.com>
2019-10-30 22:22:56 -0400

    Add test for Button

--- hackernews/src/App.test.js
+++ hackernews/src/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
-import App, { Search } from "./App";
+import App, { Search, Button } from "./App";
 
 describe("App", () => {
   it("renders without crashing", () => {
@@ -26,6 +26,20 @@ describe("Search", () => {
 
   test("has a valid snapshot", () => {
     const component = renderer.create(<Search>Search</Search>);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("Button", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Button>Give me more</Button>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test("has a valid snapshot", () => {
+    const component = renderer.create(<Button>Give me more</Button>);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
