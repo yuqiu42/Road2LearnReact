095906aa847ed6df5c075cccbdb51677141b742b
YQ <yuqiu42@gmail.com>
2019-10-31 19:51:25 -0400

    Add prop types to the components

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
+import PropTypes from "prop-types";
+
 import "./App.css";
 
 const DEFAULT_QUERY = "redux";
@@ -44,11 +46,33 @@ const Table = ({ pages, onDismiss }) => (
   </div>
 );
 
+Table.propTypes = {
+  pages: PropTypes.arrayOf(
+    PropTypes.shape({
+      objectID: PropTypes.string.isRequired,
+      author: PropTypes.string,
+      url: PropTypes.string,
+      num_comments: PropTypes.number,
+      points: PropTypes.number
+    })
+  ).isRequired,
+  onDismiss: PropTypes.func.isRequired
+};
+
 const Button = ({ onClick, className, children }) => (
   <button onClick={onClick} className={className} type="button">
     {children}
   </button>
 );
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired
+};
+Button.defaultProps = {
+  className: ""
+};
 
 class App extends Component {
   constructor(props) {
